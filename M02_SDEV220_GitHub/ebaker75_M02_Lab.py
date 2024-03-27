d89117799dcd7f9b1139f6f331aa67dde6b395f7
efbaker42 <efbaker42@gmail.com>
2024-03-26 20:27:56 -0400

    added comments to ebaker75_M02_Lab

--- M02_SDEV220_GitHub/ebaker75_M02_Lab.py
+++ M02_SDEV220_GitHub/ebaker75_M02_Lab.py
@@ -1,12 +1,14 @@
 """
 Elizabeth Baker
 GPA Checker
-This app will ask for a student's name and check if they made the Dean's List ( GPA >= 3.5) or if they made the Honor Roll (GPA >= 3.25)
+This app will ask for a student's name and check if they made the Dean's List ( GPA >= 3.5)
+or if they made the Honor Roll (GPA >= 3.25).
 """
-lName = input("Enter student's last name or ZZZZ to quit: ")
+
+lName = input("Enter student's last name or ZZZZ to quit: ") #initialize flag (last name as string)
 while lName != "ZZZZ":
-	fName = input("Enter student's first name: ")
-	gpa = float(input("Enter student's GPA: "))
+	fName = input("Enter student's first name: ") #first name as string
+	gpa = float(input("Enter student's GPA: ")) #GPA as float
 	if gpa >= 3.5:
 		print(f"{fName} {lName} made Dean's List and Honor Roll!")
 	elif gpa >= 3.25:
@@ -15,4 +17,4 @@ while lName != "ZZZZ":
 		print(f"{fName} {lName} is not qualified for Honor Roll or Den's List.")
 	lName = input("Enter next student's last name or ZZZZ to quit:")
 else:
-	print("Program complete.")
+	print("Program complete.") #inform user that program is finished
