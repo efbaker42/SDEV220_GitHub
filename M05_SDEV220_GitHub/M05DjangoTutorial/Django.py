238bea79df8c207b69daaa2383b2589145a07cc1
efbaker42 <efbaker42@gmail.com>
2024-04-27 23:28:20 -0400

    added to track progress in Django tutorial

---
+++ M05_SDEV220_GitHub/M05DjangoTutorial/Django.py
@@ -0,0 +1,13 @@
+"""
+Elizabeth Baker
+SDEV 220 35N
+24APR2024
+M05 Django Tutorial
+
+Go to Django Tutorial and do the following sections:
+-Deploy
+-Django URLs
+-Django Views
+-Introduction to HTML
+-Django ORM
+"""
