8ae0081b4c6637f3ce99ff9b7371703509f15705
efbaker42 <efbaker42@gmail.com>
2024-05-02 22:20:50 -0400

    added files for Module 7 Django tutorial

--- djangogirls/my-first-blog/blog/urls.py
+++ djangogirls/my-first-blog/blog/urls.py
@@ -3,4 +3,5 @@ from . import views #imports views from this file
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
