2d569b9c6cf86e9a6f0eba36ce63c2c05f05b6ba
efbaker42 <efbaker42@gmail.com>
2024-05-07 16:56:26 -0400

    ensuring templates, views, and models are communicating appropriately

--- SDEV_220_Final_Project_Cerulean/sheet/views.py
+++ SDEV_220_Final_Project_Cerulean/sheet/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Sheet
 
 def character_list(request):
-    return render(request, 'sheet/character_list.html', {}) #requests character list template
+    characters = Sheet.objects.filter(last_saved__lte=timezone.now()).order_by('last_saved')
+    return render(request, 'sheet/character_list.html', {'characters':characters}) #requests character list template
