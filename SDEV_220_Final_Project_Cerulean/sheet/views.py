663be6f26aa1032aece1e79f968c974516c1f64e
efbaker42 <efbaker42@gmail.com>
2024-05-08 22:55:12 -0400

    the form displays on my local machine! :D

--- SDEV_220_Final_Project_Cerulean/sheet/views.py
+++ SDEV_220_Final_Project_Cerulean/sheet/views.py
@@ -5,8 +5,9 @@ from .forms import SheetForm
 
 """def character_sheet_new(request):
     form = SheetForm()
+    characters = Sheet.objects.filter(last_saved__lte=timezone.now()).order_by('last_saved')
     return render(request, 'sheet/character_sheet.html', {'form':form})""" #url chosen since this should reference the character sheet
 
 def character_sheet(request):
-    characters = Sheet.objects.filter(last_saved__lte=timezone.now()).order_by('last_saved')
-    return render(request, 'sheet/character_sheet.html', {'form': form}) #requests character sheet template
+    form = SheetForm()
+    return render(request, 'sheet/character_sheet.html', {'form': form}) #form in curly brackets refers to form variable defined in line above
