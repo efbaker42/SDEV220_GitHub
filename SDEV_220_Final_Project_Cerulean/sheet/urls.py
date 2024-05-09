6674ae162c52bc46ff268cf53fc91b22fc3ab825
efbaker42 <efbaker42@gmail.com>
2024-05-08 21:04:18 -0400

    worked on basis of character sheet form

--- SDEV_220_Final_Project_Cerulean/sheet/urls.py
+++ SDEV_220_Final_Project_Cerulean/sheet/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.character_sheet, name='character_sheet'),
+    path('sheet/new/', views.character_sheet, name='character_sheet'), #I'm only including one url for now because I'm mostly concerned about players being able to create a sheet
 ]
--- SDEV_220_Final_Project_Cerulean/sheet/views.py
+++ SDEV_220_Final_Project_Cerulean/sheet/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Sheet
+from .forms import SheetForm
 
-#may replace with sheet later
+def character_sheet_new(request):
+    form = SheetForm()
+    return render(request, 'sheet/character_sheet.html', {'form':form}) #url chosen since this should reference the character sheet
+
+#for displaying a list of character sheets(build out later)
 def character_sheet(request):
     characters = Sheet.objects.filter(last_saved__lte=timezone.now()).order_by('last_saved')
-    return render(request, 'sheet/character_sheet.html', {'characters':characters}) #requests character sheet template
+    return render(request, 'sheet/character_sheet_list.html', {'characters':characters}) #requests character sheet template
