2ddb51dea4191342090f71255c3abb5b9675dc91
efbaker42 <efbaker42@gmail.com>
2024-05-10 12:38:52 -0400

    resolved no HTTPresponse error, continuing to work on saving sheet

--- SDEV_220_Final_Project_Cerulean/sheet/urls.py
+++ SDEV_220_Final_Project_Cerulean/sheet/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.character_sheet, name='character_sheet'),
     #path('sheet/<id>/', views.sheet_detail, name='sheet_detail'), #doesn't do anything yet; need sheet_detail view and html template
-    path('sheet/new/', views.character_sheet_new, name='sheet_new')
+    path('sheet/new/', views.character_sheet_new, name='new_sheet') #name is the name of the url; may be referenced elsewhere
 ]
