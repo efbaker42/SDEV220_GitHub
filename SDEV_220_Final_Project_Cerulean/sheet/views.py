75ee20afb0e38be4baa5142aec1f3310a3a2d081
efbaker42 <efbaker42@gmail.com>
2024-05-09 22:21:32 -0400

    attempted to add access to individual sheets as well as ability to save

--- SDEV_220_Final_Project_Cerulean/sheet/views.py
+++ SDEV_220_Final_Project_Cerulean/sheet/views.py
@@ -2,12 +2,28 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Sheet
 from .forms import SheetForm
+from django.shortcuts import redirect
 
-"""def character_sheet_new(request):
-    form = SheetForm()
-    characters = Sheet.objects.filter(last_saved__lte=timezone.now()).order_by('last_saved')
-    return render(request, 'sheet/character_sheet.html', {'form':form})""" #url chosen since this should reference the character sheet
 
+#may change later; still need way to list character sheets and edit sheets
 def character_sheet(request):
     form = SheetForm()
-    return render(request, 'sheet/character_sheet.html', {'form': form}) #form in curly brackets refers to form variable defined in line above
+    return render(request, 'sheet/character_sheet.html', {'form': form})
+
+#doesn't save yet; sheet_detail.html not fleshed out
+def character_sheet_new(request):
+    if request.method == "POST":
+        form = SheetForm(request.POST)
+        if form.is_valid():
+            sheet = form.save(commit=False)
+            sheet.author = request.user
+            sheet.last_saved = timezone.now()
+            sheet.save()
+            return redirect('sheet_detail', pk=sheet.pk) #this doesn't do anything yet because no sheet_detail page yet
+    else:
+        form = SheetForm()
+    return render(request, 'sheet/character_sheet.html', {'form': form})
+
+"""def sheet_detail(request):
+    form = SheetForm()
+    return render(request, 'sheet/sheet_detail/', {'form': form})"""
