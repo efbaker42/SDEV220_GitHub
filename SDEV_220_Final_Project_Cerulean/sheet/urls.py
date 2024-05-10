604fba2bf8f645316c45e0cf7952acf888008746
efbaker42 <efbaker42@gmail.com>
2024-05-10 11:49:08 -0400

    working on saving a sheet; getting didn't return an http response error

--- SDEV_220_Final_Project_Cerulean/sheet/urls.py
+++ SDEV_220_Final_Project_Cerulean/sheet/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.character_sheet, name='character_sheet'),
-    path('sheet/<int:pk>/', views.sheet_detail, name='sheet_detail'), #doesn't do anything yet; need sheet_detail view and html template
+    #path('sheet/<id>/', views.sheet_detail, name='sheet_detail'), #doesn't do anything yet; need sheet_detail view and html template
     path('sheet/new/', views.character_sheet_new, name='sheet_new')
 ]
--- SDEV_220_Final_Project_Cerulean/sheet/views.py
+++ SDEV_220_Final_Project_Cerulean/sheet/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Sheet
 from .forms import SheetForm
+from django.contrib import messages
 from django.shortcuts import redirect
 
 
@@ -16,14 +17,16 @@ def character_sheet_new(request):
         form = SheetForm(request.POST)
         if form.is_valid():
             sheet = form.save(commit=False)
-            sheet.author = request.user
+            #sheet.author = request.user #there is no author field required in my form
             sheet.last_saved = timezone.now()
             sheet.save()
-            return redirect('sheet_detail', pk=sheet.pk) #this doesn't do anything yet because no sheet_detail page yet
-    else:
-        form = SheetForm()
-    return render(request, 'sheet/character_sheet.html', {'form': form})
+            return redirect('character_sheet') #this is a stand-in because sheet_detail doesn't work yet
+            #return redirect('sheet_detail', pk=sheet.pk) #this doesn't do anything yet because no sheet_detail page yet
+        else:
+            #messages.error(request, "Error making sheet")
+            form = SheetForm()
+            return render(request, 'sheet/character_sheet.html', {'form': form}) #user will get same screen whether the sheet saves or not
 
-def sheet_detail(request,pk):
+def sheet_detail(request,):
     form = SheetForm()
-    return render(request, 'sheet/sheet_detail/', {'form': form})
+    return render(request, 'sheet/sheet_detail.html', {'form': form})
