5d22e9dff8eeb97f692ad8921114bddcbeac21d4
efbaker42 <efbaker42@gmail.com>
2024-05-10 14:47:25 -0400

    continued working on saving sheet

--- SDEV_220_Final_Project_Cerulean/sheet/views.py
+++ SDEV_220_Final_Project_Cerulean/sheet/views.py
@@ -13,24 +13,39 @@ def character_sheet(request):
 
 #doesn't save yet; sheet_detail.html not fleshed out
 def character_sheet_new(request):
-    request.method == "GET"
-    form = SheetForm(request.GET)
-    return render(request, 'sheet/character_sheet.html', {'form': form})
-    if request.method == "POST":
+    if request.method == "GET":
+        form = SheetForm(request.GET)
+        return render(request, 'sheet/character_sheet.html', {'form': form})
+    elif request.method == "POST":
         form = SheetForm(request.POST)
         if form.is_valid():
             sheet = form.save(commit=False)
-            #sheet.author = request.user #there is no author field required in my form, which may impact whether the form is valid
             sheet.last_saved = timezone.now()
             sheet.save()
-            #return redirect(request, 'sheet/error_message.html')
-            return redirect('sheet/character_sheet.html') #redirect uses name of url pattern #this is a stand-in because sheet_detail doesn't work yet
-            #return redirect('sheet_detail', pk=sheet.pk) #eventually, this should display the completed character sheet; this doesn't do anything yet because no sheet_detail page yet
+            return redirect('sheet_detail', pk=sheet.pk)
         else:
-            #return redirect(request, 'sheet/success_message.html') #when the no HTTPresponse error is resolved, I expect to use this for testing
             form = SheetForm()
-            return render(request, 'sheet/character_sheet.html', {'form': form}) #user will get same screen whether the sheet saves or not"""
+        return render(request, 'sheet/sheet_edit.html', {'form': form})
+#    
+#    if request.method == "POST":
+#        form = SheetForm(request.POST)
+#        if form.is_valid():
+#            sheet = form.save(commit=False)
+#            #sheet.author = request.user #there is no author field required in my form, which may impact whether the form is valid
+#            sheet.last_saved = timezone.now()
+#            sheet.save()
+#            #return redirect(request, 'sheet/error_message.html')
+#            return redirect('sheet/character_sheet.html') #redirect uses name of url pattern #this is a stand-in because sheet_detail doesn't work yet
+#            #return redirect('sheet_detail', pk=sheet.pk) #eventually, this should display the completed character sheet; this doesn't do anything yet because no sheet_detail page yet
+#        else:
+#            #return redirect(request, 'sheet/success_message.html') #when the no HTTPresponse error is resolved, I expect to use this for testing
+#            form = SheetForm()
+#            return render(request, 'sheet/character_sheet.html', {'form': form}) #user will get same screen whether the sheet saves or not"""
 
-def sheet_detail(request,):
+def sheet_detail(request,pk):
     form = SheetForm()
     return render(request, 'sheet/sheet_detail.html', {'form': form})
+
+#def sheet_list(request):
+#    sheets = Sheet.objects.filter(last_saved__lte=timezone.now()).order_by('character_name')
+#    return render(request, 'sheet/sheet_list.html', {'sheets': sheets})
