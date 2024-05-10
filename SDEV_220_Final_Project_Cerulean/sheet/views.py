1386b88f35028753e3a605d3ea7faa4f8e713c32
efbaker42 <efbaker42@gmail.com>
2024-05-10 13:01:05 -0400

    attempted to add error messages to determine issues with savingform

--- SDEV_220_Final_Project_Cerulean/sheet/views.py
+++ SDEV_220_Final_Project_Cerulean/sheet/views.py
@@ -23,10 +23,11 @@ def character_sheet_new(request):
             #sheet.author = request.user #there is no author field required in my form, which may impact whether the form is valid
             sheet.last_saved = timezone.now()
             sheet.save()
+            #return redirect(request, 'sheet/error_message.html')
             return redirect('sheet/character_sheet.html') #redirect uses name of url pattern #this is a stand-in because sheet_detail doesn't work yet
             #return redirect('sheet_detail', pk=sheet.pk) #eventually, this should display the completed character sheet; this doesn't do anything yet because no sheet_detail page yet
         else:
-            #messages.error(request, "Error making sheet") #when the no HTTPresponse error is resolved, I expect to use this for testing
+            #return redirect(request, 'sheet/success_message.html') #when the no HTTPresponse error is resolved, I expect to use this for testing
             form = SheetForm()
             return render(request, 'sheet/character_sheet.html', {'form': form}) #user will get same screen whether the sheet saves or not"""
 
